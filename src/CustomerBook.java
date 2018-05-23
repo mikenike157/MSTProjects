e9635a0bd0a0a91a0d0e616338ac052144965402
Michael Hicks <hicks74@purdue.edu>
2018-05-23 09:55:15 -0700

    First Commit

--- src/CustomerBook.java
+++ src/CustomerBook.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
  * CustomerBook: Holds the list of previous customers and runs operations manipulating that list
  */
@@ -17,17 +21,32 @@ public class CustomerBook {
         else {
             Customer customer = new Customer(name, false);
             customerList.add(customer);
+            Collections.sort(customerList, new CustomComparator());
         }
     }
     //Checks if the customer has been added to the customer list before
     //Inputs: name(Name of the desired Customer)
     //Output: true if found, if not false
     public boolean checkExists(String name) {
-        for (int i = 0; i < customerList.size(); i++) {
-            if (name.equals(customerList.get(i).name)) {
+        int min = 0;
+        int max = customerList.size()-1;
+        while (min < max) {
+            int comp = (int) Math.ceil(min + max / 2);
+            if (customerList.get(comp).name.compareTo(name) == 0) {
                 return true;
+            } else if (customerList.get(comp).name.compareTo(name) < 0) {
+                min = comp + 1;
+            } else {
+                max = comp - 1;
             }
         }
         return false;
+    }
+}
+
+class CustomComparator implements Comparator<Customer> {
+    @Override
+    public int compare(Customer o1, Customer o2) {
+        return o1.name.compareTo(o2.name);
     }
 }
